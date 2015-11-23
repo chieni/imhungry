182e7e32a00d11cb12ba0473af6d04aa7a87c098
System Administrator <root@dhcp-18-189-27-159.dyn.MIT.EDU>
2015-11-22 22:55:29 -0500

    merge

--- imHungry/routes/search.js
+++ imHungry/routes/search.js
@@ -2,7 +2,28 @@ var express = require('express');
 var router = express.Router();
 var utils = require('../utils/utils');
 var Pantry = require('../models/Pantry');
-var Recipe = require('../models/Recipe')
+var Recipe = require('../models/Recipe');
+var User = require('../models/User');
+
+
+/*
+  Require authentication on ALL access to /freets/*
+  Clients which are not logged in will receive a 403 error code.
+*/
+var requireAuthentication = function(req, res, next) {
+  if (!req.currentUser) {
+    utils.sendErrResponse(res, 403, 'Must be logged in to use this feature.');
+  } else {
+    next();
+  }
+};
+
+
+
+
+
+// Register the middleware handlers above.
+//router.all('*', requireAuthentication);
 
 router.get('/search', function(req, res) {
 	Pantry.getIngredients(req.currentUser.username, function(err, ingredients) {

fb303072d45d350e2fdc15d8c799cf1ee30c080b
System Administrator <root@dhcp-18-189-27-159.dyn.MIT.EDU>
2015-11-22 22:57:14 -0500

    merge

--- imHungry/routes/pantry.js
+++ imHungry/routes/pantry.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var utils = require('../utils/utils');
 
-<<<<<<< HEAD
+
 var User = require('../models/User');
 
 /*
@@ -17,43 +17,14 @@ var requireAuthentication = function(req, res, next) {
   }
 };
 
-/*
-  Require ownership whenever accessing a particular pantry
-  This means that the client accessing the resource must be logged in
-  as the user that the pantry is associated with. Clients who are not owners 
-  of this particular resource will receive a 404.
 
-  Why 404? We don't want to distinguish between pantries that don't exist at all
-  and pantries that exist but don't belong to the client. This way a malicious client
-  that is brute-forcing urls should not gain any information.
-*/
-var requireOwnership = function(req, res, next) {
-  if (!(req.currentUser.username === req.pantry.user)) {
-    utils.sendErrResponse(res, 404, 'Resource not found.');
-  } else {
-    next();
-  }
-};
-
-/*
-  For create and edit requests, require that the request body
-  contains a 'content' field. Send error code 400 if not.
-*/
-var requireContent = function(req, res, next) {
-  if (!req.body.content) {
-    utils.sendErrResponse(res, 400, 'Content required in request.');
-  } else {
-    next();
-  }
-};
 
 // Register the middleware handlers above.
 router.all('*', requireAuthentication);
-router.post('*', requireOwnership);
-router.post('*', requireContent);
+//router.post('*', requireOwnership);
+//router.post('*', requireContent);
 
 var Pantry = require('../models/Pantry');
->>>>>>> a27f1a93c2d5bdc0e46af3e50ab2392c4e711ceb
 
 router.get('/', function(req, res) {
   console.log("inside get");
